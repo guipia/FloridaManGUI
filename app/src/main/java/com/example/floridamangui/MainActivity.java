3fbb6f48cb4147e998d75f5da50e9fd565ec0649
guipia <guipqf@gmail.com>
2021-10-19 00:14:45 -0400

    players done username done password done check done change activity done

--- app/src/main/java/com/example/floridamangui/MainActivity.java
+++ app/src/main/java/com/example/floridamangui/MainActivity.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button loginbutton;
+    private EditText name;
+    private EditText pw;
+
     private class Player{
         private int primaryID;
         private String username;
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loginbutton = (Button) findViewById(R.id.button1);
+        name = (EditText) findViewById(R.id.textUsername);
+        pw = (EditText) findViewById(R.id.textPassword);
+
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,13 +42,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void openActivity2(){
         Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+
+        if(check(name.getText().toString(), pw.getText().toString()) == true){
+            startActivity(intent);
+            Toast.makeText(MainActivity.this, "That really is you!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Not today homie", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
-    public boolean check(String n, String pw){
+    public boolean check(String name, String pw){
         boolean check = false;
-        String name = n;
-        String passCode = pw;
+
+        String temp1 = name;
+        String temp2 = pw;
 
 
         Player player1 = new Player();
@@ -51,25 +68,31 @@ public class MainActivity extends AppCompatActivity {
         player1.bestScore = 0;
 
         Player player2 = new Player();
-        player1.primaryID = 2;
-        player1.username = "Mr. U";
-        player1.password = "HisPassword";
-        player1.fullName = "Joseph Uriarte";
-        player1.bestScore = 0;
+        player2.primaryID = 2;
+        player2.username = "Mr. U";
+        player2.password = "HisPassword";
+        player2.fullName = "Joseph Uriarte";
+        player2.bestScore = 0;
 
         Player player3 = new Player();
-        player1.primaryID = 3;
-        player1.username = "Gabriel";
-        player1.password = "AnotherPassword";
-        player1.fullName = "Gabriel Rocha";
-        player1.bestScore = 0;
+        player3.primaryID = 3;
+        player3.username = "Gabriel";
+        player3.password = "AnotherPassword";
+        player3.fullName = "Gabriel Rocha";
+        player3.bestScore = 0;
 
-        if(name.equals(player1.username) && passCode.equals(player1.password)){
 
+        if(temp1.equals(player1.username) && temp2.equals(player1.password)){
+            check = true;
         }
+        else if(name.equals(player2.username) && pw.equals(player2.password)){
+            check = true;
+        }
+        else if(name.equals(player3.username) && pw.equals(player3.password)){
+            check = true;
+        }
+        return check;
     }
 
-   // public void sendMessage(View view) {
-       // Intent intent = new Intent()
     }
 //}
