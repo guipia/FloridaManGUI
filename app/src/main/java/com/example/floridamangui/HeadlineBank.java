d34442322f829826ac5f28dfb8d76b96de91774e
guipia <guipqf@gmail.com>
2021-10-20 09:50:00 -0400

    players done username done password done check done change activity done HeadlineBank in progress

--- app/src/main/java/com/example/floridamangui/HeadlineBank.java
+++ app/src/main/java/com/example/floridamangui/HeadlineBank.java
@@ -1,4 +1,65 @@
 package com.example.floridamangui;
+import java.util.HashMap;
 
 public class HeadlineBank {
+    public static void headlines()
+        {
+        HashMap<String, String> storyObjects = new HashMap<String, String>();
+
+            storyObjects.put("headline1", "FLORIDA MAN GETS BEAT UP BY ...");
+            storyObjects.put("headline2", "FLORIDA MAN CLINGS TO ... SPEEDING DOWN HIGHWAY");
+            storyObjects.put("headline3", "FLORIDA MAN TRIES TO SHOOT ..., ... SHOOTS HIM INSTEAD");
+            storyObjects.put("headline4", "FLORIDA MAN USES 4 MILLION IN COVID RELIEF TO BUY ...");
+            storyObjects.put("headline5", "NAKED FLORIDA MAN DRINKS 2 LITERS OF ..., BURNS DOWN HOUSE BAKING ... ON GEORGE FOREMAN GRILL");
+            storyObjects.put("headline6", "NAKED FLORIDA MAN THROWS ... AT CARS");
+            storyObjects.put("headline7", "FLORIDA MAN CHARGED WITH ASSAULT WITH A DEADLY WEAPON AFTER THROWING ... THROUGH WENDY'S DRIVE-THRU WINDOW");
+            storyObjects.put("headline8", "FLORIDA MAN STEALS A CAR, REALIZES A ... IS IN IT, DROPS ... OFF SAFELY, AND MAKES HIS GET AWAY");
+            storyObjects.put("headline9", "THOUSANDS OF GUN OWNERS IN FLORIDA PLANNING TO 'SHOOT DOWN' ...");
+            storyObjects.put("headline10","FLORIDA MAN TRIES TO STEAL GAMESTOP WHILE WEARING ... ON HIS HEAD");
+
+            storyObjects.put("answer1", "The easter bunny");
+            storyObjects.put("answer2", "Semitruck");
+            storyObjects.put("answer3", "Puppy, puppy");
+            storyObjects.put("answer4", "Lamborghini");
+            storyObjects.put("answer5", "Vodka, cookies");
+            storyObjects.put("answer6", "Rocks");
+            storyObjects.put("answer7", "Alligator");
+            storyObjects.put("answer8", "Baby");
+            storyObjects.put("answer9", "Hurricane irma");
+            storyObjects.put("answer10", "Transparent bag");
+
+            storyObjects.put("incorrectAnswer1", "The police");
+            storyObjects.put("incorrectAnswer2", "Ronald McDonald");
+            storyObjects.put("incorrectAnswer3", "5 lower school kids");
+            storyObjects.put("incorrectAnswer4", "Car");
+            storyObjects.put("incorrectAnswer5", "Motorcycle");
+            storyObjects.put("incorrectAnswer6", "Ambulance");
+            storyObjects.put("incorrectAnswer7", "Old lady, old lady");
+            storyObjects.put("incorrectAnswer8", "Alligator, alligator");
+            storyObjects.put("incorrectAnswer9", "Obama, Obama");
+            storyObjects.put("incorrectAnswer10", "A dog");
+            storyObjects.put("incorrectAnswer11", "Illegal animals");
+            storyObjects.put("incorrectAnswer12", "Lifetime supply of whey protein");
+            storyObjects.put("incorrectAnswer13", "Rum, cookies");
+            storyObjects.put("incorrectAnswer14", "Beer, cake");
+            storyObjects.put("incorrectAnswer15", "Water, cake");
+            storyObjects.put("incorrectAnswer16", "Bricks");
+            storyObjects.put("incorrectAnswer17", "Computers");
+            storyObjects.put("incorrectAnswer18", "Random Nokia phones");
+            storyObjects.put("incorrectAnswer19", "Granede");
+            storyObjects.put("incorrectAnswer20", "A rock");
+            storyObjects.put("incorrectAnswer21", "Bluetooth speakers blasting Spongebob's theme song");
+            storyObjects.put("incorrectAnswer22", "Dog");
+            storyObjects.put("incorrectAnswer23", "Cat");
+            storyObjects.put("incorrectAnswer24", "Land mine");
+            storyObjects.put("incorrectAnswer25", "Big bird");
+            storyObjects.put("incorrectAnswer26", "Barney the dinosaur");
+            storyObjects.put("incorrectAnswer27", "Wild turkey");
+            storyObjects.put("incorrectAnswer28", "The queen's guard hat");
+            storyObjects.put("incorrectAnswer29", "Pink beret");
+            storyObjects.put("incorrectAnswer30", "A defective mask");
+
+
+            //OVERRIDE EQUALS AND HASHCODE
+    }
 }
